014d27bc6fd38b766ac531761ee72809b7b52344
Arnab-31 <57807670+Arnab-31@users.noreply.github.com>
2022-05-05 13:37:31 +0530

    Word break

---
+++ String/72. Word Break(M).cpp
@@ -0,0 +1,37 @@
+//https://practice.geeksforgeeks.org/problems/word-break1352/1
+
+class Solution
+{
+public:
+
+    int solve(string A, string cur, int id, unordered_set<string>st){
+    
+        
+        if(st.find(cur) != st.end()){
+            if(id == A.length()){
+                return 1;   
+            }else{
+                if(solve(A, "", id, st) ||  solve(A, cur + A[id], id + 1, st))
+                    return 1;
+                else
+                    return 0;
+            }
+        }
+        
+        if(id == A.length())
+            return 0;
+            
+        return solve(A, cur + A[id], id+1, st);
+    }
+    
+    int wordBreak(string A, vector<string> &B) {
+        //code here
+        unordered_set<string>st;
+        for(auto i : B)
+            st.insert(i);
+        
+        int ans = solve(A, "" , 0, st);
+        
+        return ans;
+    }
+};
