5d6633fcb9b133bc2831e38a9d83790ebf2ea82a
Arnab-31 <57807670+Arnab-31@users.noreply.github.com>
2022-05-22 17:39:55 +0530

    Minimum Platforms new approach

--- Greedy/245. Minimum Platforms(M#).cpp
+++ Greedy/245. Minimum Platforms(M#).cpp
@@ -32,3 +32,45 @@ int findPlatform(int arr[], int dep[], int n)
     
     return count;
 }
+
+
+
+//Second Approach with priority queue with TC - O(nlogn)
+
+class Solution{
+    public:
+    //Function to find the minimum number of platforms required at the
+    //railway station such that no train waits.
+    int findPlatform(int arr[], int dep[], int n)
+    {
+    	if(n == 1)
+    	    return 1;
+    	// Your code here
+    	vector<pair<int,int>>v;
+    	
+    	for(int i=0;i<n;i++){
+    	    v.push_back({arr[i], dep[i]});
+    	}
+    	sort(v.begin(), v.end());  //Sort based on arrival time
+    	
+    	
+    	priority_queue<int, vector<int>, greater<int>> pq;
+    	pq.push(v[0].second);
+    	int count = 1;   //number of platforms
+    	
+    	for(int i = 1;i<n;i++){
+    	    
+    	    while(!pq.empty() && pq.top() < v[i].first)  //remove the trains whose departure time is earlier than the arrival time of the current train
+    	        pq.pop();
+    	        
+    	    if(pq.size() < count){  //Check if there is any empty station
+    	        pq.push(v[i].second);
+    	    }else{   //If not then increase the number of station
+    	        count++;
+    	        pq.push(v[i].second);
+    	    }
+    	}
+    	
+    	return count;
+    }
+};
